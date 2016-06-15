024599f03e42b58b0b39d661ce3014f3a39a2497
Danilo Horta <danilo.horta@gmail.com>
2016-06-15 17:01:20 +0100

    fully working?

--- limix_reader/reader/test/test_bed.py
+++ limix_reader/reader/test/test_bed.py
@@ -3,11 +3,12 @@ from os.path import join
 from os.path import realpath
 
 from numpy import array
-# from numpy import asarray
+from numpy import asarray
+from numpy import atleast_2d
 from numpy import nan
 from numpy.testing import assert_equal
 from numpy.testing import assert_array_equal
-# from numpy.testing import assert_string_equal
+from numpy.testing import assert_string_equal
 
 def test_read():
     from ..bed import reader
@@ -73,34 +74,27 @@ def test_read():
     assert_array_equal(G['2_3', '1_snp2'], R[5,1])
     assert_array_equal(G['2_3', '1_snp3'], R[5,2])
 
-    # g = G['1_1', ['1_snp1', '1_snp2', '1_snp3']]
-    # print(g)
-    # assert_array_equal(G[0,:], array([ 0.,  2.,  2.]))
-#     assert_array_equal(G[1,:], array([  2.,  nan,   1.]))
-#     assert_array_equal(G[2,:], array([ nan,   1.,   1.]))
-#     assert_array_equal(G[3,:], array([  2.,   2.,  nan]))
-#     assert_array_equal(G[4,:], array([  2.,   2.,  nan]))
-#     assert_array_equal(G[5,:], array([ 2.,  2.,  0.]))
-#
-#     assert_array_equal(G[:,0], array([  0.,   2.,  nan,   2.,   2.,   2.]))
-#     assert_array_equal(G[:,1], array([  2.,  nan,   1.,   2.,   2.,   2.]))
-#     assert_array_equal(G[:,2], array([  2.,   1.,   1.,  nan,  nan,   0.]))
-#
-#     assert_string_equal(stable['family_id']['1_1'], '1')
-#     assert_string_equal(stable['family_id']['2_1'], '2')
-#     assert_string_equal(stable['family_id']['1_2'], '1')
-#
-#     assert_string_equal(stable['individual_id']['1_2'], '2')
-#     assert_string_equal(stable['individual_id']['2_1'], '1')
-#
-#     assert_equal(mtable['base_pair_position']['1_snp1'], 1.0)
-#     assert_equal(mtable['base_pair_position']['1_snp2'], 2.0)
-#     assert_equal(mtable['base_pair_position']['1_snp3'], 3.0)
-#
-#     R = array([[ 0.,  2.,  2.],
-#                [  2.,  nan,   1.],
-#                [ nan,   1.,   1.],
-#                [  2.,   2.,  nan],
-#                [  2.,   2.,  nan],
-#                [ 2.,  2.,  0.]])
-#     assert_array_equal(asarray(G), R)
+    assert_array_equal(G['1_1', ['1_snp1', '1_snp2', '1_snp3']], atleast_2d(R[0,:]))
+    assert_array_equal(G['1_2', ['1_snp1', '1_snp2', '1_snp3']], atleast_2d(R[1,:]))
+    assert_array_equal(G['1_3', ['1_snp1', '1_snp2', '1_snp3']], atleast_2d(R[2,:]))
+    assert_array_equal(G['2_1', ['1_snp1', '1_snp2', '1_snp3']], atleast_2d(R[3,:]))
+    assert_array_equal(G['2_2', ['1_snp1', '1_snp2', '1_snp3']], atleast_2d(R[4,:]))
+    assert_array_equal(G['2_3', ['1_snp1', '1_snp2', '1_snp3']], atleast_2d(R[5,:]))
+
+    assert_array_equal(G[['1_1', '1_2', '1_3', '2_1', '2_2', '2_3'], '1_snp1'], atleast_2d(R[:,0]).T)
+    assert_array_equal(G[['1_1', '1_2', '1_3', '2_1', '2_2', '2_3'], '1_snp2'], atleast_2d(R[:,1]).T)
+    assert_array_equal(G[['1_1', '1_2', '1_3', '2_1', '2_2', '2_3'], '1_snp3'], atleast_2d(R[:,2]).T)
+
+
+    assert_string_equal(stable['family_id']['1_1'], '1')
+    assert_string_equal(stable['family_id']['2_1'], '2')
+    assert_string_equal(stable['family_id']['1_2'], '1')
+
+    assert_string_equal(stable['individual_id']['1_2'], '2')
+    assert_string_equal(stable['individual_id']['2_1'], '1')
+
+    assert_equal(mtable['base_pair_position']['1_snp1'], 1.0)
+    assert_equal(mtable['base_pair_position']['1_snp2'], 2.0)
+    assert_equal(mtable['base_pair_position']['1_snp3'], 3.0)
+
+    assert_array_equal(asarray(G), R)
