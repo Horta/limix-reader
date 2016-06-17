ba47da69336669b17f88c4b448f698b1054f9030
Danilo Horta <danilo.horta@gmail.com>
2016-06-17 16:38:55 +0100

    raise error for contracting matrices

--- limix_reader/util/copyto.py
+++ limix_reader/util/copyto.py
@@ -3,5 +3,11 @@ from numpy import isnan
 def copyto_nans(from_sidx, from_midx, F, to_sidx, to_midx, T):
     for i in range(len(from_sidx)):
         for j in range(len(from_midx)):
-            if isnan(T[to_sidx[i], to_midx[j]]):
-                T[to_sidx[i], to_midx[j]] = F[from_sidx[i], from_midx[j]]
+            tos, tom = to_sidx[i], to_midx[j]
+            froms, fromm = from_sidx[i], from_midx[j]
+            if isnan(T[tos, tom]):
+                T[tos, tom] = F[froms, fromm]
+            else:
+                if (not isnan(F[froms, fromm]) and
+                        T[tos, tom] != F[froms, fromm]):
+                    raise ValueError("Matrices values are contradicting.")
