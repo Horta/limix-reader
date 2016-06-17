5ac5a146381b0f6a4a65a0c7b29bdcc5e984eb42
Danilo Horta <danilo.horta@gmail.com>
2016-06-17 02:33:32 +0100

    mergint table

--- limix_reader/table/table.py
+++ limix_reader/table/table.py
@@ -2,12 +2,13 @@ from collections import Iterable
 
 from numpy import where
 from numpy import union1d
+from numpy import intersect1d
 
 from pandas import DataFrame
 
 from .column import Column
-from ..util import npy2py_type
 from ..util import make_sure_list
+from ..util import isscalar
 
 class Table(object):
     def __init__(self, df=None):
@@ -32,39 +33,23 @@ class Table(object):
 
     @index_values.setter
     def index_values(self, values):
+        name = self._df.index.name
         self._df.index = values
+        self._df.index.name = name
 
     @property
     def columns(self):
         return self._df.columns.values
 
-    def set_index_value(self, old_val, new_val):
-        otype = npy2py_type(type(old_val))
-        ntype = npy2py_type(type(new_val))
-
-        index_name = self._df.index.name
-        values = self._df.index.values
-        i = where(values == old_val)[0][0]
-
-        if otype != ntype:
-            values = values.astype(ntype)
-
-        values[i] = new_val
-
-        self._df.index = values
-
-        self._df.index.name = index_name
-
-    def __getitem__(self, args):
-        if isinstance(args, bytes):
-            return Column(args, self.index_values, self._df[args].values)
-        if isinstance(args, Iterable):
-            return TableView(column_names=args)
-        return Column(args, self.index_values, self._df[args].values)
+    def __getitem__(self, colname):
+        if isscalar(colname):
+            return Column(colname, self.index_values, self._df[colname].values)
+        return Table(self._df[colname])
 
     def loc(self, index_values):
-        index_values = make_sure_list(index_values)
-        return TableView(self, index_values)
+        pass
+        # index_values = make_sure_list(index_values)
+        # return TableView(self, index_values)
 
     @property
     def shape(self):
@@ -78,11 +63,8 @@ class Table(object):
     def dtypes(self):
         return self._df.dtypes
 
-    def __array__(self, *args, **kwargs):
-        kwargs = dict(kwargs)
-        if 'index_values' not in kwargs:
-            return self._df.__array__()
-        return self._df.loc[kwargs['index_values']].__array__()
+    def __array__(self):
+        return self._df.__array__()
 
     def __repr__(self):
         return repr(self._df)
@@ -91,71 +73,86 @@ class Table(object):
         return bytes(self._df)
 
     def merge(self, that):
-        from .mtable import MTable
-        return MTable(self, that)
+        indcol = "__indicator_column__"
+        df = self._df.merge(that._df, how='outer', left_index=True,
+                            right_index=True, indicator=indcol)
+        cols = intersect1d(self._df.columns, that._df.columns)
+        for c in cols:
+            cl = c + '_x'
+            cr = c + '_y'
+            for i in df.index:
+                if df[indcol][i] == 'right_only':
+                    df.loc[i, cl] = df.loc[i, cr]
 
-class MTable(object):
-    def __init__(self, lhs, rhs):
-        self._lhs = lhs
-        self._rhs = rhs
+        for c in cols:
+            del df[c + '_y']
+        df.rename(columns={(c+'_x'):c for c in cols}, inplace=True)
+        del df[indcol]
 
-        if lhs.index_name != rhs.index_name:
-            raise ValueError("Index names differ.")
-
-    @property
-    def index_name(self):
-        return self._lhs.index_name
-
-    @index_name.setter
-    def index_name(self, v):
-        self._lhs.index_name = v
-        self._rhs.index_name = v
-
-    @property
-    def index_values(self):
-        return union1d(self._lhs.index_values, self._rhs.index_values)
-
-    @property
-    def columns(self):
-        return union1d(self._lhs.columns, self._rhs.columns)
-
-    def __getitem__(self, colname):
-        try:
-            c0 = self._lhs.__getitem__(colname)
-        except KeyError:
-            return self._rhs.__getitem__(colname)
-
-        try:
-            c1 = self._rhs.__getitem__(colname)
-        except KeyError:
-            return c0
-
-        return c0.merge(c1)
-        # return Column(colname, self.index_values, self._df[colname].values)
-    #
-    # def loc(self, index_values):
-    #     index_values = make_sure_list(index_values)
-    #     return TableView(self, index_values)
-    #
-    # @property
-    # def shape(self):
-    #     return self._df.shape
-    #
-    # @property
-    # def ndim(self):
-    #     return 2
-    #
-    # @property
-    # def dtypes(self):
-    #     return self._df.dtypes
-    #
-    # def __array__(self, index_values=None):
-    #     if index_values is None:
-    #         return self._df.__array__()
-    #     return self._df.loc[index_values].__array__()
-    #
-    # def __repr__(self):
-    #     return repr(self._df)
-    #
-    # def __str__(self):
-    #     return bytes(self._df)
+        return Table(df)
+#
+# class MTable(object):
+#     def __init__(self, lhs, rhs):
+#         self._lhs = lhs
+#         self._rhs = rhs
+#
+#         if lhs.index_name != rhs.index_name:
+#             raise ValueError("Index names differ.")
+#
+#     @property
+#     def index_name(self):
+#         return self._lhs.index_name
+#
+#     @index_name.setter
+#     def index_name(self, v):
+#         self._lhs.index_name = v
+#         self._rhs.index_name = v
+#
+#     @property
+#     def index_values(self):
+#         return union1d(self._lhs.index_values, self._rhs.index_values)
+#
+#     @property
+#     def columns(self):
+#         return union1d(self._lhs.columns, self._rhs.columns)
+#
+#     def __getitem__(self, colname):
+#         try:
+#             c0 = self._lhs.__getitem__(colname)
+#         except KeyError:
+#             return self._rhs.__getitem__(colname)
+#
+#         try:
+#             c1 = self._rhs.__getitem__(colname)
+#         except KeyError:
+#             return c0
+#
+#         return c0.merge(c1)
+#         # return Column(colname, self.index_values, self._df[colname].values)
+#     #
+#     # def loc(self, index_values):
+#     #     index_values = make_sure_list(index_values)
+#     #     return TableView(self, index_values)
+#     #
+#     # @property
+#     # def shape(self):
+#     #     return self._df.shape
+#     #
+#     # @property
+#     # def ndim(self):
+#     #     return 2
+#     #
+#     # @property
+#     # def dtypes(self):
+#     #     return self._df.dtypes
+#     #
+#     # def __array__(self, index_values=None):
+#     #     if index_values is None:
+#     #         return self._df.__array__()
+#     #     return self._df.loc[index_values].__array__()
+#     #
+#     # def __repr__(self):
+#     #     return repr(self._df)
+#     #
+#     # def __str__(self):
+#     #     return bytes(self._df)
--- limix_reader/table/test/test_table.py
+++ limix_reader/table/test/test_table.py
@@ -1,82 +1,103 @@
-# from numpy import object_
-# from numpy import nan
-# from numpy import array
-# from numpy import logical_not as lnot
-#
-# from numpy.testing import assert_array_equal
-# from numpy.testing import assert_equal
-# from numpy.testing import assert_raises
-#
-# from pandas import isnull
-#
-# from ..table import Table
-# from ..column import Column
-#
-# def test_adding_columns():
-#     t = Table()
-#
-#     labels =['sample01', 'sample02', 'sample03']
-#     values = [34.3, 2.3, 103.4, -030.]
-#     c = Column('height', labels, values)
-#     t.add(c)
-#
-#     labels =['sample02', 'sample03']
-#     values = ['doce', 'cogumelo']
-#     c = Column('comida', labels, values)
-#     t.add(c)
-#
-#     t.index_name = 'sample_id'
-#
-#     assert_array_equal(t.as_matrix()[:,0], array([2.3, 103.4, 34.3]))
-#
-#     v = t.as_matrix()[:,1]
-#     v = v[lnot(isnull(v))]
-#
-#     r = array(['doce', 'cogumelo', nan], dtype=object_)
-#     r = r[lnot(isnull(r))]
-#
-#     assert_array_equal(v, r)
-#
-# def test_column_indexing():
-#
-#     t = Table()
-#
-#     labels =['sample01', 'sample02', 'sample03']
-#     values = [34.3, 2.3, 103.4, -030.]
-#     c = Column('height', labels, values)
-#     t.add(c)
-#
-#     labels =['sample02', 'sample03']
-#     values = ['doce', 'cogumelo']
-#     c = Column('comida', labels, values)
-#     t.add(c)
-#
-#     t.index_name = 'sample_id'
-#
-#     c = t['comida']
-#
-#     assert_equal(c['sample03'], array(['cogumelo']))
-#     with assert_raises(KeyError):
-#         print(c['sample05'])
-#
-# def test_get_table_by_columns():
-#
-#     t = Table()
-#
-#     labels =['sample01', 'sample02', 'sample03']
-#     values = [34.3, 2.3, 103.4, -030.]
-#     c = Column('height', labels, values)
-#     t.add(c)
-#
-#     labels =['sample02', 'sample03']
-#     values = ['doce', 'cogumelo']
-#     c = Column('comida', labels, values)
-#     t.add(c)
-#
-#     t.index_name = 'sample_id'
-#
-#     c = t['comida']
-#
-#     assert_equal(c['sample03'], array(['cogumelo']))
-#     with assert_raises(KeyError):
-#         print(c['sample05'])
+from numpy import object_
+from numpy import nan
+from numpy import array
+from numpy import asarray
+from numpy import logical_not as lnot
+
+from numpy.testing import assert_array_equal
+from numpy.testing import assert_equal
+from numpy.testing import assert_raises
+from numpy.ma import masked_invalid
+
+from pandas import isnull
+
+from ..table import Table
+from ..column import Column
+
+def test_adding_columns():
+    t = Table()
+
+    labels =['sample01', 'sample02', 'sample03']
+    values = [34.3, 2.3, 103.4]
+    c = Column('height', labels, values)
+    t.add(c)
+
+    labels =['sample02', 'sample03']
+    values = ['doce', 'cogumelo']
+    c = Column('comida', labels, values)
+    t.add(c)
+
+    t.index_name = 'sample_id'
+
+    a = array([[34.3, nan], [2.3, 'doce'],
+               [103.4, 'cogumelo']], dtype=bytes)
+    assert_array_equal(asarray(t).astype(bytes), a)
+
+def test_column_indexing():
+
+    t = Table()
+
+    labels =['sample01', 'sample02', 'sample03']
+    values = [34.3, 2.3, 103.4]
+    c = Column('height', labels, values)
+    t.add(c)
+
+    labels =['sample02', 'sample03']
+    values = ['doce', 'cogumelo']
+    c = Column('comida', labels, values)
+    t.add(c)
+
+    t.index_name = 'sample_id'
+
+    c = t['comida']
+
+    assert_equal(c['sample03'], array(['cogumelo']))
+    with assert_raises(KeyError):
+        print(c['sample05'])
+
+def test_change_index():
+    t = Table()
+
+    labels =['sample01', 'sample02', 'sample03']
+    values = [34.3, 2.3, 103.4]
+    c = Column('height', labels, values)
+    t.add(c)
+
+    labels =['sample02', 'sample03']
+    values = ['doce', 'cogumelo']
+    c = Column('comida', labels, values)
+    t.add(c)
+    t.index_name = 'sample_id'
+    t.index_values = [1, 2, 3]
+    t.index_name = 'hola'
+
+def test_merge_tables():
+    t1 = Table()
+
+    labels =['sample01', 'sample02', 'sample03']
+    values = [34.3, 2.3, 103.4]
+    c = Column('height', labels, values)
+    t1.add(c)
+
+    labels =['sample02', 'sample03']
+    values = ['doce', 'cogumelo']
+    c = Column('comida', labels, values)
+    t1.add(c)
+    t1.index_name = 'sample_id'
+
+    t2 = Table()
+
+    labels =['sample01', 'sample09', 'sample02', 'sample03']
+    values = [34.3, -23, 2.3, 103.4]
+    c = Column('height', labels, values)
+    t2.add(c)
+
+    assert_equal(t12['height']['sample01'], 34.3)
+    assert_equal(t12['height']['sample02'], 2.3)
+    assert_equal(t12['height']['sample03'], 103.4)
+    assert_equal(t12['height']['sample09'], -23.0)
+
+    assert_equal(bytes(t12['comida']['sample01']), 'nan')
+    assert_equal(t12['comida']['sample02'], 'doce')
+    assert_equal(t12['comida']['sample03'], 'cogumelo')
+    assert_equal(bytes(t12['comida']['sample09']), 'nan')
